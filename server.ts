70308a582d6689d539f083ff1bff104890015c41
agepting <bitbucket@gepting.de>
2024-06-11 23:00:57 +0200

    feat: adjusted for heroku

--- server.ts
+++ server.ts
@@ -44,12 +44,13 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const host = '0.0.0.0';
+  const port = parseInt(process.env['PORT'] || '4000');
 
   // Start up the Node server
   const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+  server.listen(port, host, () => {
+    console.log(`Node Express server listening on http://${host}:${port}`);
   });
 }
 
